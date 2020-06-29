0f3b8bf90b44fd72b46e31c125a48b36d094bc1e
Alvaro Lopes <alvaro40@gmail.com>
2020-06-29 06:01:09 -0400

    :sparkles: challenge done

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,59 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  category: string;
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category,
+    title,
+    type,
+    value,
+  }: Request): Promise<Transaction> {
+    if (!(type === 'income' || type === 'outcome')) {
+      throw new AppError('Type of transaction should be income or outcome.');
+    }
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance.');
+    }
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const createdTransaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionRepository.save(createdTransaction);
+
+    return createdTransaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,17 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const returnedTransaction = await transactionRepository.findOne(id);
+
+    if (!returnedTransaction) throw new AppError('Transaction not found');
+
+    await transactionRepository.remove(returnedTransaction);
   }
 }
 
